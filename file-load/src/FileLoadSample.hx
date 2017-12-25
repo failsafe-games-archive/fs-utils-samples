830a15da023e8ce3c4a5f346cc87197eae2ffcaa
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2017-12-25 15:47:13 -0500

    Update sample

--- file-load/src/FileLoadSample.hx
+++ file-load/src/FileLoadSample.hx
@@ -1,24 +1,32 @@
 import fs.core.File;
 
+import tink.RunLoop;
+
 using tink.CoreApi;
 
 @await class FileLoadSample {
 
     public function new() {
         trace('FileLoadSample');
-        
+
         test1();        
     }
 
     @async public function test1() {
         trace('1111');
 
-        //var text = @await File.load('google.com/allo.txt');
-        //trace(text.substr(0, 10));
-
-        File.load('assets/test.ogg').handle((outcome) -> switch(outcome) {
-            case Success(bytes) : trace(bytes.length);
+        File.load('assets/test3').handle((outcome) -> switch(outcome) {
+            case Success(bytes) : 
+                trace('THREAD 2', tink.concurrent.Thread.current == tink.concurrent.Thread.MAIN);
+                trace(bytes.length);
             case Failure(error) : trace('Cannot load!!!');
+        });
+
+        File.downloadString('http://www.sample-videos.com/csv/Sample-Spreadsheet-10-rows.csv').handle((outcome) -> switch(outcome) {
+            case Success(text) : 
+                trace('THREAD 3', tink.concurrent.Thread.current == tink.concurrent.Thread.MAIN);
+                trace(text.length);
+            case Failure(error) : trace('Cannot download!!!');
         });
         
         trace('22222');
